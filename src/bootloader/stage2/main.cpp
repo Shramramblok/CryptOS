ce4bb2b4466967a41edc2c4b4e414b64fc94f83b
Shramramblok <roeilustig1@gmail.com>
2023-08-06 22:20:23 +0300

    Fixing printf: -Rewriting printf because of many issues. -One of the annoying issues was the diffrence between (foo)*var and  *(foo*)var.

--- src/bootloader/stage2/main.cpp
+++ src/bootloader/stage2/main.cpp
@@ -4,11 +4,7 @@
 
 void _cdecl cstart_(uint16_t bootDriveNumber)
 {
-    puts("Hello, World!\r\n");
-    printf("Hello from Crypt %c %% %c %s\r\n", 'O', 'S', "printf!");
-    printf("Our Numbers: %d %i %x %p %o %hd %hi %hhu %hhd\r\n", 5680, -5050, 0xbeeb, 0xeeee, 021354, (short)55, (short)-34, (unsigned)20, (unsigned)-10);
-    printf("Big Ones: %ld %lx %lld %llx\r\n", -100000001l, 0xdef88deful, 40300200100ll, 0xbeebeebeebeebeebull);
-    while (1){
+    printf("Hello from Crypt%c%c using a %d%% working %s\r\n", 'O', 'S', 100, "printf!");
         
     }
     
--- src/bootloader/stage2/stdio.cpp
+++ src/bootloader/stage2/stdio.cpp
@@ -45,109 +45,111 @@ void puti(int64_t value, uint32_t base)
     putu(value, base);
 }
 
+void putn_printf(int* &argp, uint8_t size, bool sign, uint32_t base, int incsize)
+{
+    if (size < sizeof(int))
+        size = sizeof(int);
+    switch (size)
+    {
+    case 1:
+    case 2:
+        if (sign)
+            puti(*(int16_t*)argp, base);
+        else
+            putu(*(uint16_t*)argp, base);
+        break;
+    case 4:
+        if (sign)
+            puti(*(int32_t*)argp, base);
+        else
+            putu(*(uint32_t*)argp, base);
+        break;
+    case 8:
+        if (sign)
+            puti(*(int64_t*)argp, base);
+        else
+            putu(*(uint64_t*)argp, base);
+        break;
+    }
+
+    argp += incsize * size / sizeof(int);
+}
+
 void _cdecl printf(const char* fmt, ...){
     int* argp = (int*)&fmt;  // stack is aligned to the int datatype
     int incsize = sizeof(fmt) / sizeof(int);
     argp += incsize;  // points to the second argument
-    int curst = PRINTF_NORMAL;
-    int curln = PRINTFLN_DEFAULT;
-    int numbase = 10;  // starts of as decimal
-    int sign = false;  // starts of as unsigned
+    uint8_t size = sizeof(int);
 
     while (*fmt){
-        switch (curst){
-            case PRINTF_NORMAL: 
-                 switch (*fmt){
-                    case '%': curst = PRINTF_LENGTH;
-                              break;
-
-                    default: putc(*fmt);  // if character != %: print it (normal letter)
-                             break;
-                 }
-                 break;
-            
-            case PRINTF_LENGTH:
-                 switch (*fmt){
-                    case 'h': curln = PRINTFLN_SHORT;
-                              curst = PRINTF_LNSHORT;
-                              break;
-
-                    case 'l': curln = PRINTFLN_LONG;
-                              curst = PRINTF_LNLONG;
-                              break; 
-
-                    default: goto PRINTF_SPECLBL;
-                 }
-                 break;
-
-            case PRINTF_LNSHORT:
-                 if (*fmt == 'h'){
-                    curln = PRINTFLN_SHORT_SHORT;
-                    curst = PRINTF_SPECIFIER;
-                 }
-                 else{
-                    goto PRINTF_SPECLBL;
-                 }
-
-            case PRINTF_LNLONG:
-                 if (*fmt == 'l'){
-                    curln = PRINTFLN_LONG_LONG;
-                    curst = PRINTF_SPECIFIER;
-                 }
-                 else{
-                    goto PRINTF_SPECLBL;
-                 }
-
-            case PRINTF_SPECIFIER:
-            PRINTF_SPECLBL:
-                switch (*fmt){
-                    case 'c': putc((char)*argp);  // specifier "c" = specifies a character
-                              argp += incsize;
-                              break;
-
-                    case 's': puts(*(char**)argp);  // specifier "s" = specifies a string
-                              argp += incsize;
-                              break;
-
-                    case '%': putc('%');  // specifier "%" = specifies a '%' character
-                              argp += incsize;
-                              break;
-
-                    case 'd': 
-                    case 'i': numbase = 10;  // specifiers "i/d" = specifies a signed decimal 
-                              sign = true;
-                              puti(*argp, numbase);
-                              argp += incsize;
-                              break;
-
-                    case 'u': numbase = 10;  // specifier "u" = specifies an unsigned decimal 
-                              sign = false;
-                              putu(*argp, numbase);  
-                              argp += incsize;
-                              break;
-                    
-                    case 'x':
-                    case 'X':
-                    case 'p': numbase = 16;  // specifiers "x/X/p" = specifies an unsigned hexadecimal 
-                              sign = false;
-                              putu(*argp, numbase);
-                              argp += incsize;
-                              break;
-
-                    case 'o': numbase = 8;  // specifier "o" = specifies an unsigned octal 
-                              sign = false;
-                              putu(*argp, numbase);
-                              argp += incsize;
-                              break;
-                    
-                    default: break;
-                }   
-                curst = PRINTF_NORMAL;
-                curln = PRINTFLN_DEFAULT;  
-                numbase = 10;
-                sign = false;
+        if (*fmt == '%')
+        {
+            fmt++;
+            switch (*fmt)
+            {
+            case 'l':
+                if (*(fmt + 1) == 'l')
+                {
+                    size = 8;
+                    fmt++;
+                }
+                else
+                {
+                    size = 4;
+                }
+                fmt++;
                 break;
+                
+            case 'h':
+                if (*(fmt + 1) == 'h')
+                {
+                    size = 1;
+                    fmt++;
+                }
+                else
+                {
+                    size = 2;
+                }
+                fmt++;
+                break;
+            }
+            
+            switch (*fmt)
+            {
+            
+            case 'c':
+                putc(*argp);
+                argp += incsize;
+                break;
+            case 's':
+                puts((char*)*argp);
+                argp += incsize;
+                break;
+            case 'd':
+            case 'i':
+                putn_printf(argp, size, true, 10, incsize);
+                break;
+            case 'u':
+                putn_printf(argp, size, false, 10, incsize);
+                break;
+            case 'p':
+            case 'X':
+            case 'x':
+                putn_printf(argp, size, false, 16, incsize);
+                break;
+            case 'o':
+                putn_printf(argp, size, false, 8, incsize);
+                break;
+            case '%':
+                putc('%');
+                break;
+            default:
+                break;
+            }
+        } else {
+            putc(*fmt);
         }
+        size = sizeof(int);
         fmt += incsize; // move to the next character in the format string
     }
 }
